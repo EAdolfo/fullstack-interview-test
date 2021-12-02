b7f3daf598bbbd0d413254b592920fb686051a00
Adolfo Galindo <eadolfogalindo@gmail.com>
2021-12-02 00:00:56 -0600

    Added: navigator component

---
+++ Gitfrontend/src/app/navigator/navigator.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navigator',
+  templateUrl: './navigator.component.html',
+  styleUrls: ['./navigator.component.css']
+})
+export class NavigatorComponent implements OnInit {
+
+  constructor(
+    public router: Router,
+  ) { }
+
+  ngOnInit() {
+  }
+
+}
